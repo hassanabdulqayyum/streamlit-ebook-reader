7ac941dfc2d44affa0487a12b80523f850f27aa4
hassanabdulqayyum <hassanabdulqayyum@hotmail.com>
2024-12-21 22:49:58 +0500

    Update read.py

--- read.py
+++ read.py
@@ -285,7 +285,7 @@ def main():
 
     /* Hide the Streamlit style elements (hamburger menu, header, footer) */
     
-    #MainMenu {visibility: hidden;}
+    header {visibility: hidden;}
     footer {visibility: hidden;}
 
     /* Responsive font sizes for mobile devices */
